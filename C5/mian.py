ac17a8f55d131c44a577c21804c3f461826afeaf
s25140 <s25140@pjwstk.edu.pl>
2023-04-15 12:14:12 +0200

    Initial commit #2

--- C5/mian.py
+++ C5/mian.py
@@ -12,16 +12,25 @@ data = {}
 
 def sendEMail(recieverEMail):
     student = data[recieverEMail]
-    sender = ""
+    try:
+        sender = "" #here write your e-mail
+    except TypeError:
+        print("Exception: Add your e-mail in line 16")
+        raise SystemExit
     msg = MIMEText(f"Dostałeś ocenę: {student['ocena']}")
     msg['Subject'] = "Ocena wystawiona!"
     msg['From'] = sender
     msg['To'] = ', '.join(recieverEMail)
     smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(sender, "")
+    try:
+        smtp_server.login(sender, "")#here write App password
+    except TypeError:
+        print("Exception: Add your e-mail App password in line 26")
+        raise SystemExit
     smtp_server.sendmail(sender, recieverEMail, msg.as_string())
     smtp_server.quit()
     student['status'] = "MAILED"
+    print("E-mail sent")
 
 def getGrade(points):
     points = int(points)
@@ -42,18 +51,19 @@ FileName = "plik.txt"
 with open(FileName,'r') as f:
     for line in f:
         lineData = line.split(",")
-        data[lineData[0]] = {"imię" : lineData[1],
+        data[lineData[0]] = {"imie" : lineData[1],
                              "nazwisko" : lineData[2],
                              "punkty" : lineData[3].replace("\n",''),
                              "ocena" : lineData[4].replace("\n",'') if len(lineData)>4 else getGrade(lineData[3]),
                              "status" : lineData[5].replace("\n",'') if len(lineData)>5 else "GRADED"
                              }
 #print(data)
-def updateData():
+def updateData(): # write grade, send mail, set MAILED and write to file
     for key in data.keys():
         student = data[key]
-        if student["status"]!="GRADED" or student["status"]!="MAILED":
+        if student["status"]!="GRADED" and student["status"]!="MAILED":
             student["ocena"] = getGrade(student["punkty"])
+        elif student["status"]!="MAILED":
             sendEMail(key)
 
     with open(FileName, 'w') as f:
@@ -64,8 +74,27 @@ def updateData():
 
 command = None
 while command != '-quit':
-    command = input("Commands:\n-delete example@gmail.com\n-update example@gmail.com <imię> <nazwisko> <punkty>\n-quit")
+    command = input("Commands:\n-delete example@gmail.com\n-update example@gmail.com <imię> <nazwisko> <punkty>\n-add example@gmail.com <imię> <nazwisko> <punkty>\n-quit\n")
     if not command.startswith("-"):
         print("Not a command")
         continue
-    if command.startswith("-delete"):
+    if command.startswith("-quit"):
+        raise SystemExit
+    Email = command.split(" ")[1]
+    if command.startswith("-delete") and len(command.split(" "))==2:
+        data.pop(Email)
+        updateData()
+        print(f"{Email} removed")
+    elif command.startswith("-update") or command.startswith("-add") and len(command.split(" "))==5:
+        args = command.split(" ")
+        data[Email] = {
+            "imie": args[2],
+            "nazwisko": args[3],
+            "punkty": args[4].replace("\n", ''),
+            "ocena": getGrade(args[4]),
+            "status": "GRADED"
+        }
+        print (f"Added/Updated: {data[Email]}")
+        updateData()
+    else:
+        print("Wrong command")
